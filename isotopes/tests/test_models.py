31a54e418349641578e3b157d222b7234d2c5c38
Air <air@Air.local>
2018-07-22 09:34:53 +0200

    Changed U235, U236

--- isotopes/tests/test_models.py
+++ isotopes/tests/test_models.py
@@ -9,14 +9,14 @@ class IsotopeModelTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        Isotope.object.create(name='U235',
-                              z_number=92,
-                              a_number=235,
-                              natural_abund=0.007204,
-                              binding_energy=1783.86105944496,
-                              atomic_mass=235.043930131)
+        Isotope.objects.create(name='U236',
+                               z_number=90,
+                               a_number=233,
+                               natural_abund=0.0,
+                               binding_energy=771.47012801421,
+                               atomic_mass=233.041582278)
 
     def test_decay_mode(self):
         isotope = Isotope.objects.get(id=1)
-        decay_mode = isotope.decay_mode
-        self.assertEqual()
+        decay_mode = isotope.decay_mode()
+        self.assertEqual(decay_mode, (0, 1))
